6072fa27598d7b58ae9e197f370c06f5283d8367
Abigael Kirwa <abigael.kirwa@strathmore.edu>
2024-04-15 12:08:46 +0300

    feat(create):added create route and controller

--- server/index.js
+++ server/index.js
@@ -2,6 +2,11 @@ const express = require('express')
 const app = express()
 const port = 3500
 
+const taskRoute = require('./routes/taskRoutes')
+
+app.use(express.json())
+app.use('/task', taskRoute)
+
 app.get('/', (req, res)=>{
     res.send("Hello World")
 })
