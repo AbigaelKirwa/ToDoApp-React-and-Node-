b363a17c602ada57a1c08261910cad84bab697f2
Abigael Kirwa <abigael.kirwa@strathmore.edu>
2024-04-16 17:03:37 +0300

    feat(read):integrate read backend functionality with front-end to enable viewing, also added cors to backend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
+import ViewToDo from './components/ViewToDo';
+import Heading from './components/Heading';
+import BoxCover from './components/BoxCover';
 
 function App() {
+  const API_URL = "http://localhost:3500/"
   return (
     <div className="App">
-      Hello World
+      <BoxCover>
+        <Heading/>
+        <ViewToDo  API_URL = {API_URL}/>
+      </BoxCover>
     </div>
   );
 }
